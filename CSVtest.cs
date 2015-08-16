a82630ec6d73a260f4dc5a9d0b33c6335b581740
Austin Cathey <thepeoplescoder@gmail.com>
2015-08-16 04:37:29 -0700

    Added CSVtest.cs.

---
+++ CSVtest.cs
@@ -0,0 +1,29 @@
+﻿// CSV test.
+
+using System;
+using System.Text;
+using CSV;
+
+class CSVtest
+{
+
+    public static void Main(string[] args)
+    {
+        CSVWriter cw;
+        CSVReader cr;
+
+        cw = new CSVWriter("test.csv");
+        cw.Write("Sample text");
+        cw.Write(23424);
+        cw.Write("This is a string\"that contains a double quote character.");
+        cw.Close();
+
+        cr = new CSVReader("test.csv");
+        object x;
+        while ((x = cr.ReadNext()) != null)
+        {
+            Console.WriteLine(x);
+        }
+        cr.Close();
+    }
+}
